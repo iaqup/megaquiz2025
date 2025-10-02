b0a2470ef503f6b23104185d43b82385f96f94fc
Jacek-Prokop1 <jacek12prokop@gmail.com>
2025-10-02 09:10:39 +0200

    Update rejestracja.php

--- megaquiz2025-main/rejestracja.php
+++ megaquiz2025-main/rejestracja.php
@@ -13,6 +13,7 @@ $conn = new mysqli("localhost","root","","quizy");
         <input type="email" name="email" placeholder="Email" maxlength="60" required>
         <input name="user" placeholder="Nazwa użytkownika" minlength="6" maxlength="16" required>
         <input type="password" name="haslo" placeholder="Hasło" minlength="8" maxlength="16" required>
+        <input type="password" name="haslo2" placeholder="Powtorz Hasło" minlength="8" maxlength="16" required>
         <input type="submit" name="submit" value="Zarejestruj">
     </form>
 
@@ -21,21 +22,27 @@ $conn = new mysqli("localhost","root","","quizy");
         $email = $_POST['email'];
         $users = $_POST['user'];
         $haslo = $_POST['haslo'];
+        $haslo2 = $_POST['haslo2'];
 
-        if (!empty($email) && !empty($users) && !empty($haslo)) {
-            $conn->query("INSERT INTO `uzytkownicy` (`id`, `nazwa`, `email`, `haslo`, `potwierdzony`) VALUES (NULL, '$users', '$email', '$haslo', 1)");
-            $user_id = $conn->insert_id;
-            $conn->close();
+        if (!empty($email) && !empty($users) && !empty($haslo) && !empty($haslo2)) {
 
-            // Automatyczne logowanie po rejestracji
-            session_start();
-            $_SESSION['user'] = $users;
-            $_SESSION['email'] = $email;
-            $_SESSION['id'] = $user_id;
-            $_SESSION['admin'] = 0;
+            if($haslo == $haslo2){
+                $haslo_hash = password_hash($haslo, PASSWORD_DEFAULT);
+                $conn->query("INSERT INTO `uzytkownicy` (`id`, `nazwa`, `email`, `haslo`, `potwierdzony`) VALUES (NULL, '$users', '$email', '$haslo_hash', 1)");
+                $user_id = $conn->insert_id;
+                $conn->close();
 
-            header("Location: index.php");
-            exit();
+                session_start();
+                $_SESSION['user'] = $users;
+                $_SESSION['email'] = $email;
+                $_SESSION['id'] = $user_id;
+                $_SESSION['admin'] = 0;
+
+                header("Location: index.php");
+                exit();
+            } else{
+                echo '<div style="color:red;">Hasła muszą być takie same!</div>';
+            }
         } else {
             echo '<div style="color:red;">Wypełnij wszystkie pola!</div>';
         }
