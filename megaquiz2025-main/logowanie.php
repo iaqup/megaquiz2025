e059d3762825a25e48922995ef18cc638b54eec2
Jacek-Prokop1 <jacek12prokop@gmail.com>
2025-10-08 14:08:06 +0200

    Update logowanie.php

--- megaquiz2025-main/logowanie.php
+++ megaquiz2025-main/logowanie.php
@@ -6,7 +6,6 @@ if ($conn->connect_error) {
     die("Błąd połączenia z bazą: " . $conn->connect_error);
 }
 $conn->set_charset("utf8mb4"); 
-
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -16,34 +15,36 @@ $conn->set_charset("utf8mb4");
     <title>Logowanie</title>
 </head>
 <body>
-        <nav class="hotbar">
+<nav class="hotbar">
   <div class="left">
-    <a href="index.php"><a href="index.php"><img src="logoo.png" alt="skibidi" width="60" height="50"></a></a>
+    <a href="index.php"><img src="logoo.png" alt="skibidi" width="60" height="50"></a>
   </div>
   <div class="right">
     <a href="rejestracja.php">Zarejestruj się</a>
   </div>
 </nav>
-    <h1>Zaloguj się</h1>
-    <form action="logowanie.php" method="post">
-        <input name="user" placeholder="Email / nazwa użytkownika" minlength="6" maxlength="60" required>
-        <input type="password" name="haslo" placeholder="Hasło" minlength="8" maxlength="16" required>
-        <input type="submit" name="submit" value="Zaloguj">
-    </form>
+
+<h1>Zaloguj się</h1>
+<form action="logowanie.php" method="post">
+    <input name="user" placeholder="Email / nazwa użytkownika" minlength="6" maxlength="60" required>
+    <input type="password" name="haslo" placeholder="Hasło" minlength="8" maxlength="16" required>
+    <input type="submit" name="submit" value="Zaloguj">
+</form>
 
 <?php
 if (isset($_POST['submit'])) {
     $user = trim($_POST['user']);
     $haslo = $_POST['haslo'];
 
+    $user = $conn->real_escape_string($user);
+
     if (str_contains($user, '@')) {
-        $stmt = $conn->prepare("SELECT * FROM `uzytkownicy` WHERE `email` = ?");
+        $sql = "SELECT * FROM `uzytkownicy` WHERE `email` = '$user' LIMIT 1";
     } else {
-        $stmt = $conn->prepare("SELECT * FROM `uzytkownicy` WHERE `nazwa` = ?");
+        $sql = "SELECT * FROM `uzytkownicy` WHERE `nazwa` = '$user' LIMIT 1";
     }
-    $stmt->bind_param("s", $user);
-    $stmt->execute();
-    $result = $stmt->get_result();
+
+    $result = $conn->query($sql);
 
     if ($result && $result->num_rows > 0) {
         $row = $result->fetch_assoc();
@@ -66,8 +67,6 @@ if (isset($_POST['submit'])) {
     } else {
         echo '<div style="color:red;">Nie znaleziono użytkownika!</div>';
     }
-
-    $stmt->close();
 }
 $conn->close();
 ?>
